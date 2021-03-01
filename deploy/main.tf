02837315148902de7448af6550390dae18bcbf09
Tony Vattathil <avattathil@gmail.com>
2021-03-01 15:33:34 -0800

    Refactor deploy default to tfc/remote execution

--- deploy/main.tf
+++ deploy/main.tf
@@ -7,12 +7,7 @@
 ######################################
 terraform {
   required_version = ">= 0.13"
-
   backend "remote" {}
-}
-
-provider "aws" {
-  region = var.region
 }
 
 resource "random_pet" "name" {
@@ -20,13 +15,31 @@ resource "random_pet" "name" {
   length = 1
 }
 
+locals {
+  name = random_pet.name.id
+}
+
+######################################
+# Create VPC
+######################################
+
+module "aurora_vpc" {
+  source            = "aws-quickstart/vpc/aws"
+  region            = var.region
+  name              = local.name
+  cidr              = "10.0.0.0/16"
+  public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
+  private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
+}
+
 ######################################
 # Create Aurora DB
 ######################################
 
 module "aurora" {
-  source = "../../modules/aurora"
-  region = var.region
-  name   = random_pet.name.id
-  vpc_id = "" #add the VPC ID you wish the database to be built in.
+  depends_on = [module.aurora_vpc]
+  source     = "aws-quickstart/rds-aurora/aws"
+  region     = var.region
+  name       = local.name
+  vpc_id     = module.aurora_vpc.vpc_id
 }
--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@
 ######################################
 terraform {
   required_version = ">= 0.13"
+
+  backend "remote" {}
 }
 
 provider "aws" {
@@ -18,31 +20,13 @@ resource "random_pet" "name" {
   length = 1
 }
 
-locals {
-  name = random_pet.name.id
-}
-
-######################################
-# Create VPC
-######################################
-
-module "aurora_vpc" {
-  source            = "aws-quickstart/vpc/aws"
-  region            = var.region
-  name              = local.name
-  cidr              = "10.0.0.0/16"
-  public_subnets    = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20", "10.0.240.0/22", "10.0.244.0/22"]
-  private_subnets_A = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.232.0/22", "10.0.236.0/22"]
-}
-
 ######################################
 # Create Aurora DB
 ######################################
 
 module "aurora" {
-  depends_on = [module.aurora_vpc]
-  source     = "../../modules/aurora"
-  region     = var.region
-  name       = local.name
-  vpc_id     = module.aurora_vpc.vpc_id
+  source = "./modules/aurora"
+  region = var.region
+  name   = random_pet.name.id
+  vpc_id = "" #add the VPC ID you wish the database to be built in.
 }
